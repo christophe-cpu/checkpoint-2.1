72c2a15167f57bd6a94f4e60b87bdacadab1090f
Your Namechristophe-cpu <christophe-michaud1@orange.fr>
2021-08-04 12:18:05 +0200

    heroe ok

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import'./HeroCard.css';
 
-function HeroCard({heroe}) {
+function HeroCard({heroe, deleteHeroe}) {
     return (
         <div className="HeroCard">
-            <Link to ={{pathname: `/$ heroe.id`}} >
+            <Link to ={{pathname: `/${heroe.name}`}} >
                 <h2>{heroe.name}</h2>
             </Link>
-            <img className="heroe-picture" src={heroe.image} alt="heroe.name" />
-            
+            <img className="image" src={heroe.image} alt="heroe.name" />
+            <button onClick = {()=> deleteHeroe(heroe.name)}>
+                Delete Heroe
+            </button>   
         </div>
     )
 }
--- src/components/HeroDetails.jsx
+++ src/components/HeroDetails.jsx
@@ -1,15 +1,26 @@
-import React, {useState,UseEffect} from 'react';
+import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 import{ Link} from'react-router-dom';
 
 
 
 
-function HeroDetails() {
+
+function HeroDetails(props){
+
+        const [height, setHeight] = useState();
+        const heroeName = props.match.params.name
+    
+        useEffect(() => {
+            axios
+            .get(`https://apis.wilders.dev/wild-games/games/${heroeName}`)
+            .then((res) => {
+                setHeight(res.data.height)
+            })
+        }, [heroeName])
     return (
         <div className='HeroDetails'>
-            
-            
+            <Link to={{pathname: `/`}} >Go back to homepage </Link>
         </div>
     )
 }
